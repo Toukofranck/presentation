de3cac403b1123d6bd0895989ef9fad44140314e
franck touko <francktouko04@gmail.com>
2025-06-27 16:53:33 +0100

    monprojetportfolio

---
+++ app.js
@@ -0,0 +1,8 @@
+const typed = new typed('.multiple',{
+    strings:["Développeur Web","web Designer","formateur Web"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+
+})
